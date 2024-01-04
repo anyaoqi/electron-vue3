46e00b9294a025922bf08990aeac6c550619b3d0
杨奇 <yangq1@hsit.com.cn>
2024-01-04 11:28:45 +0800

    修改element-plus

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,13 @@ import pinia from '@/pinia'
 
 // Element-plus
 import ElementPlus from 'element-plus'
-import locale from "element-plus/lib/locale/lang/zh-cn"
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
-app.use(ElementPlus, {locale})
+app.use(ElementPlus)
 
 // element-ui图标：注册全局组件
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
